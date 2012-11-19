0413bf67a419eed83f362b8a1e4baca3cb93a479
Branko Vukelic <branko@brankovukelic.com>
2012-11-19 02:11:29 +0100

    Fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     long_description="django-international is a set of reusable components "
     "and data that can be used in Django projects that need to handle country "
     "and currency data. It contains forms, models, fixtures, and data in "
-    "form usable directly by Python programs."
+    "form usable directly by Python programs.",
     version='0.0.1',
     packages=['international'],
     author='Monwara LLC',
