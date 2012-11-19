ac0fa2ed6edcb515bfff13f07827465265a163f5
Branko Vukelic <branko@brankovukelic.com>
2012-11-19 01:24:35 +0100

    Added distutils files

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='django-international',
+    description='Country and currency data for Django projects',
+    long_description='Models, forms, and data for use with projects that '
+    'require country- and currency-related functionality.'
+    version='0.0.1',
+    packages=['international'],
+    author='Monwara LLC',
+    author_email='branko@monwara.com',
+    url='https://bitbucket.org/monwara/django-international',
+    download_url='https://bitbucket.org/monwara/django-international/downloads',
+    license='BSD',
+    classifiers = [
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
+
+
