64ac38372af9cb2f44584b4c5595b6bbc07ac627
Branko Vukelic <branko@brankovukelic.com>
2012-11-19 02:05:11 +0100

    Updated long desc

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from distutils.core import setup
 setup(
     name='django-international',
     description='Country and currency data for Django projects',
-    long_description='Models, forms, and data for use with projects that '
-    'require country- and currency-related functionality.',
+    long_description="django-international is a set of reusable components "
+    "and data that can be used in Django projects that need to handle country "
+    "and currency data. It contains forms, models, fixtures, and data in "
+    "form usable directly by Python programs."
     version='0.0.1',
     packages=['international'],
     author='Monwara LLC',
