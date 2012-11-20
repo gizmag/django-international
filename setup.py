fc7cf9c3324c4af14bc009679ce174c883154d94
Branko Vukelic <branko@brankovukelic.com>
2012-11-20 16:21:11 +0100

    Updated version and added news

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='django-international',
     description='Country and currency data for Django projects',
     long_description=open('README.rst').read(),
-    version='0.0.1',
+    version='0.0.2',
     packages=['international'],
     author='Monwara LLC',
     author_email='branko@monwara.com',
