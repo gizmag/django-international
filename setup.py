30e9fe17b487fdae35f7165c3b8d1670b8da8143
Branko Vukelic <branko@brankovukelic.com>
2013-04-03 01:48:11 +0200

    Bumped version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='django-international',
     description='Country and currency data for Django projects',
     long_description=open('README.rst').read(),
-    version='0.0.5',
+    version='0.0.6',
     packages=['international'],
     package_data={
         '': ['fixtures/*.json'],
