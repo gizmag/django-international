067033f5eaf68a5408ed38a5ae63b1578b4c2d3f
Branko Vukelic <branko@brankovukelic.com>
2013-04-03 00:50:27 +0200

    Fixed to distutils to include the fixtures

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(
     name='django-international',
     description='Country and currency data for Django projects',
     long_description=open('README.rst').read(),
     version='0.0.5',
-    packages=find_packages(),
-    include_package_data=True,
+    packages=['international'],
+    package_data={
+        '': ['fixtures/*.json'],
+    },
     author='Monwara LLC',
     author_email='branko@monwara.com',
     url='https://bitbucket.org/monwara/django-international',
